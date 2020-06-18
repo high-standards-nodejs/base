089aed05dbfcff305ad0bcc76ac334d7f4257023
Oliver Lorenz <mail@oliverlorenz.com>
2020-06-18 13:31:39 +0200

    feat: adds nested template functionality

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ const fs = require('fs');
 const path = require('path');
 const readline = require('readline');
 const mkdirp = require('mkdirp');
+const template = require('es6-template-strings');
+const pkgDir = require('pkg-dir');
 
 if (!process.env.INIT_CWD) process.env.INIT_CWD = process.cwd();
 if (process.cwd() === process.env.INIT_CWD) process.exit(0);
 
 const initPackageJsonPath = path.join(process.env.INIT_CWD, 'package.json');
-const highStandardsFilePath = path.join(process.env.INIT_CWD, '.highstandards');
+const highStandardsFilePath = path.join(process.env.INIT_CWD, '.highstandards', 'accepted');
 
 function getOwnPackageJson() {
     return JSON.parse(
@@ -19,6 +21,10 @@ function getInitiatingProjectPackageJson() {
     return JSON.parse(
         fs.readFileSync(initPackageJsonPath)
     );
+}
+
+function getProjectRoot() {
+    return process.env.INIT_CWD;
 }
 
 function writeInitiatingProjectPackageJson(packageJson) {
@@ -53,12 +59,44 @@ function checkAcceptedHighStandards() {
         output: process.stdout
     });
 
-    rl.question('Are you aware that "high-standards" libraries can add / modify / remove files in your project. (yes/NO)', function(answer) {
+    rl.question('Are you aware that "high-standards" libraries can add / modify / remove files in your project? (yes/NO) ', function(answer) {
         if (answer.toLowerCase() === 'yes') {
-            writeFile(highStandardsFilePath, '', true);
+            createAcceptFile();
+        } else {
+            console.log('exit');
+            process.exit(0);
         }
         rl.close();
     });
+}
+
+function createAcceptFile() {
+    mkdirp.sync(path.dirname(highStandardsFilePath));
+    writeFile(highStandardsFilePath, '', true);
+}
+
+
+function getTemplate(packageDir, filePath, context) {
+    const templatePath = getTemplatePath(packageDir, filePath);
+    const fileContent = fs.readFileSync(templatePath).toString();
+    return template(fileContent, context);
+}
+
+function getTemplatePath(packageDir, filePath) {
+    const initPackageTemplatePath = path.join(
+        pkgDir.sync(process.env.INIT_CWD),
+        '.highstandards',
+        filePath
+    );
+    if (fs.existsSync(initPackageTemplatePath)) return initPackageTemplatePath;
+    
+    const packageTemplatePath = path.join(
+        pkgDir.sync(packageDir),
+        '.highstandards',
+        filePath
+    );
+    if (fs.existsSync(packageTemplatePath)) return packageTemplatePath;
+    throw new Error(`"${filePath}" not found`);
 }
 
 module.exports = {
@@ -67,4 +105,6 @@ module.exports = {
     writeInitiatingProjectPackageJson,
     writeFile,
     checkAcceptedHighStandards,
+    getTemplate,
+    getProjectRoot,
 }
